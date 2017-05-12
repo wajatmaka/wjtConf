0e83190a8abccbb8943dca25b3540bd7d9341e1b
Dwiyan Galuh Wijatmiko <dwiyan.wijatmiko@1rstwap.com>
2017-05-12 16:36:55 +0700

    tambah konfigurasi squid

---
+++ Squid/sarg/miniwebservice-sarg.sh
@@ -0,0 +1,32 @@
+[root@squidshare sbin]# cat /usr/bin/squid-report
+#!/bin/bash
+#
+# Dwiyan Galuh 
+# Mini Web service for sarg
+#
+#
+
+STATUS=$1
+
+case $STATUS in
+      start | START )
+      cd /var/www/html/reports
+      python -m SimpleHTTPServer 8000 > /dev/null 2>&1 &
+      echo $! > /var/run/sarg-web/sarg.pid
+      ;;
+      stop | STOP )
+      kill -9 $(cat /var/run/sarg-web/sarg.pid);
+      ;;
+      status | STATUS )
+      DATA=`ps -ef | grep python | grep SimpleHTTPServer | wc -l`
+      if [ $DATA -gt 0 ]
+      then
+         echo "Service Hidup"
+      else
+         echo "Service mati"
+      fi
+      ;;
+      * )
+      echo $"Usage: $0 {start|stop|status}"
+      exit 1
+esac
